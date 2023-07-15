8724e40aa25efee185110c0e460b8628428a1398
NicerWang <nicer_wang@qq.com>
2023-07-16 00:01:56 +0800

    Fix: #1

--- Scweet/utils.py
+++ Scweet/utils.py
@@ -9,6 +9,7 @@ import chromedriver_autoinstaller
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 
@@ -147,7 +148,8 @@ def init_driver(headless=True, proxy=None, show_images=False, option=None):
         options.add_experimental_option("prefs", prefs)
     if option is not None:
         options.add_argument(option)
-    driver = webdriver.Chrome(options=options, executable_path=chromedriver_path)
+    service = Service(executable_path=chromedriver_path)
+    driver = webdriver.Chrome(options=options, service=service)
     driver.set_page_load_timeout(100)
     return driver
 
